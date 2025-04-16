10fd6f79e6cc4d457186b66247c804d98896e342
matthew <meriematthew01@gmail.com>
2025-04-16 18:13:15 +0300

    adds props mini project

--- personal-blog-site/src/App.jsx
+++ personal-blog-site/src/App.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import Header from './components/Header';
 import About from './components/About';
 import ArticleList from './components/ArticleList';
-import articles from './data/blog'; // Assuming you have a data folder with blog.js
+import articles from './data/blog'; 
 
 const App = () => {
   return (
     <div>
       <Header name="My Personal Blog" />
       <About 
-        image="path/to/image.jpg" // Replace with actual image path or a default
+        image="path/to/image.jpg"
         about="This is a blog about my personal experiences and thoughts."
       />
       <ArticleList articles={articles} />
